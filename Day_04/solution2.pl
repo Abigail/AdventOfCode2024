df6374da7cc15a12310a89e3fdc28f6b7110cd48
Abigail <abigail@abigail.freedom.nl>
2024-12-07 22:07:39 +0100

    Slight improvement

--- Day_04/solution2.pl
+++ Day_04/solution2.pl
@@ -11,7 +11,7 @@ use experimental 'lexical_subs';
 
 @ARGV = shift // "input";
 
-my $input = [map {[/./g, "."]} <>];
+my $input   = [map {[split //]} <>];  # Includes "\n"
 
 my $skip_ns = @{$$input [0]} - 1;
 my $skip_nw =    $skip_ns + 1;
@@ -32,7 +32,7 @@ my $solution_2 = 0;
       S.{$skip_ne}A.{$skip_ne}M.{$skip_ne}X     # NE -> SW
   )
   (?{$solution_1 ++}) (*FAIL)
-/x;
+/xs;
 
 
 / (?: M.M .{$skip_ne} A .{$skip_ne} S.S |
@@ -40,7 +40,7 @@ my $solution_2 = 0;
       S.M .{$skip_ne} A .{$skip_ne} S.M |
       S.S .{$skip_ne} A .{$skip_ne} M.M )
   (?{$solution_2 ++}) (*FAIL)
-/x;
+/xs;
 
 say "Solution 1: $solution_1";
 say "Solution 2: $solution_2";
